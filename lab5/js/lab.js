2d6fc0452a06d51be89b9d83ae2a5a218f12a58c
astelus <156488983+astelus87@users.noreply.github.com>
2024-04-25 16:15:58 -0700

    lab 5 JS and html

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author:    Samantha Lung
+ * Created:   05.25.2024
+ * 
+ * (c) Public Domain
+ **/
 
-// Constants
+//Define Variables
+let carMake = "Toyota"
+let carName = "Corolla Hybrid";
+let carColor = "Silver"
+let carYear = 2021;
+let currentYear = 2024;
+let carAge = currentYear - carYear;
 
-// Functions
+document.writeln("Car Make: " + carMake + "<br>");
+document.writeln("Car Name: " + carName + "<br>");
+document.writeln("Car Color: " + carColor + "<br>")
+document.writeln("Car Year: " + carYear + "<br>");
+document.writeln("Current Year: " + currentYear + "<br>");
+document.writeln("Car Age: " + carAge + "<br>");
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
