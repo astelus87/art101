c41ffec7f35233ae957162eaae5aa28d4e55c395
astelus <156488983+astelus87@users.noreply.github.com>
2024-05-12 17:47:00 -0700

    lab 9 on homepage + update

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -11,4 +11,29 @@ $("#buttonChallenge").click(function(){
   $("#challenge").toggleClass("special");
 });
 
+// add button to problem section
+$("#problems").append("<button id='buttonProblems'>Press Me</button>");
 
+// add a click listener to the problem button
+$("#buttonProblems").click(function(){
+  // now add (or subtract) the "special" class to the section
+  $("#problems").toggleClass("special");
+});
+
+// add button to problem section
+$("#reflections").append("<button id='buttonReflections'>Press Me</button>");
+
+// add a click listener to the problem button
+$("#buttonReflections").click(function(){
+  // now add (or subtract) the "special" class to the section
+  $("#reflections").toggleClass("special");
+});
+
+// add button to problem section
+$("#results").append("<button id='buttonResults'>Press Me</button>");
+
+// add a click listener to the problem button
+$("#buttonResults").click(function(){
+  // now add (or subtract) the "special" class to the section
+  $("#results").toggleClass("special");
+});
